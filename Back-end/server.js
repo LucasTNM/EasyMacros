7c50c2abf7c84396dee6da22b3b5b82470a22cf0
LucasTNM <148718831+LucasTNM@users.noreply.github.com>
2025-03-25 18:33:34 -0300

    cors config

--- Back-end/server.js
+++ Back-end/server.js
@@ -7,13 +7,17 @@ import chatRoutes from './Routes/chatRoutes.js'
 
 dotenv.config();
 
-// Conectar ao banco de dados
 connectDB();
 
 const app = express();
 app.use(express.json());
 
-app.use(cors());
+app.use(
+    cors({
+      origin: "http://localhost:3000",
+      credentials: true,
+    })
+  );
 
 app.use('/api/user', userRoutes);
 app.use('/api/chat', chatRoutes);
