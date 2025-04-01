323c3dc179ea7e9194f8678fc89ca63a771897f4
LucasTNM <148718831+LucasTNM@users.noreply.github.com>
2025-04-01 19:35:46 -0300

    Mudança no regex da nova senha

--- Back-end/controller/userController.js
+++ Back-end/controller/userController.js
@@ -387,10 +387,10 @@ export const createNewPassword = async (req, res) => {
       return res.status(404).json({ message: "Usuário não encontrado." });
     }
     
-    const passwordRegex = /^(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/;
+    const passwordRegex = /^(?=.*[A-Z])(?=.*[!@#$%^&*])(?=.{8,})/;
     if (!passwordRegex.test(senha)) {
       return res.status(400).json({
-        message: "A senha deve ter no mínimo 8 caracteres, incluindo uma letra maiúscula, um número e um símbolo especial."
+        message: "A senha deve ter no mínimo 8 dígitos, um caractere especial e uma letra maiúscula"
       });
     }
 
